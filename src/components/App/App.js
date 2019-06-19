cb57a14009c385e684a16233e72fbea1aff1354c
Unknown <m.kudamatsu@gmail.com>
2019-06-19 15:48:53 +0900

    Set the header and the background #9
    Move App.js into the components/ folder; Add the background image file; Add relevant CSS files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+      </div>
+    </div>
+  );
+}
+
+export default App;
