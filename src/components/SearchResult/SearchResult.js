9b3576541576449952572b84c0630f2113ef19ab
Unknown <m.kudamatsu@gmail.com>
2019-07-04 09:21:13 +0900

    Add FontAwesome headphone icon for listening sample tracks #19
    It won't show up for tracks without previews

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -16,15 +16,23 @@ class SearchResult extends React.Component {
     event.preventDefault();
   }
   render() {
+    let listenIcon;
+    if (this.props.track.previewURL) {
+      listenIcon = (
+        <a onClick={this.handleListen}>
+          <i className="fas fa-headphones-alt"></i>
+        </a>
+      );
+    } else {
+      listenIcon = <div></div>;
+    }
     return (
       <div className="Track">
         <div className="Track-information">
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <a onClick={this.handleListen}>
-          <img src="./headphones-alt-solid.svg" alt="LISTEN"/>
-        </a>
+        {listenIcon}
         <a className="Track-action"
            onClick={this.handleClick}>+</a>
       </div>
